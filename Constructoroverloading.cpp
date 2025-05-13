5f59c1272733920a720e9d0f4e9e4d4dcd8c4922
Azizahaul <azizahauliarhamid@gmail.com>
2025-05-13 16:08:03 +0700

    Membuat definisi nim dan nama

--- Constructoroverloading.cpp
+++ Constructoroverloading.cpp
@@ -12,3 +12,16 @@ class mahasiswa{
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa :: mahasiswa(){
+}
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; //definis hanya NIM
+}
+mahasiswa :: mahasiswa(int iNama){
+    nama = iNama; //definisi hanya NAMA
+}
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;//definisi NIM dan NAMA
+    nama = iNama;
+}
